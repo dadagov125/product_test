f4171edd49526071377cb531cc80119c427ffce3
Magomed Dadagov <grozniy.info@gmail.com>
2022-09-08 17:25:35 +0300

    clean architecture

---
+++ lib/application/application.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/screens/home_screen.dart';
+
+class Application extends StatefulWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
